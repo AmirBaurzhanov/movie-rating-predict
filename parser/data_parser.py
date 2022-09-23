53082136b5d173fa2f2651e1c14292a9f087612b
Amir Baurzhanov <amirbaurzhanov@MacBook-Air.local>
2022-09-23 16:16:01 +0600

    another bug fixed

--- parser/data_parser.py
+++ parser/data_parser.py
@@ -20,15 +20,16 @@ def get_data(links, list):
 		data = {
 			"title": soup.findAll('h1', class_='sc-b73cd867-0')[0].text,
 			"year": soup.findAll('a', class_='ipc-link ipc-link--baseAlt ipc-link--inherit-color sc-8c396aa2-1 WIUyh')[0].text,
-			"certificate": soup.findAll('a', class_='ipc-link ipc-link--baseAlt ipc-link--inherit-color sc-8c396aa2-1 WIUyh')[0].text,
+			"certificate": soup.findAll('span', class_='sc-8c396aa2-2 itZqyK')[0].text,
 			"rating": soup.findAll('span', class_='sc-7ab21ed2-1 jGRxWM')[0].text,
 			"description": soup.findAll('span', class_='sc-16ede01-2 gXUyNh')[0].text,
 			"director": soup.select('li.ipc-metadata-list__item:first-child > div > ul > li > a')[0].text,
 			"writer": [soup.findAll('a', class_='ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link')[0].text,
 			soup.findAll('a',class_='ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link')[1].text],
 			"actors": [soup.findAll('a', class_='sc-36c36dd0-1 QSQgP')[i].text for i in range(len(soup.findAll('a', class_='sc-36c36dd0-1 QSQgP')))],
-			"genres": soup.findAll('a', class_='ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link')[0].text,
-		}	
+			"genres": [soup.findAll('a', class_='ipc-chip__text')[i].text for i in range(len(soup.findAll('a', class_='ipc-chip__text')))],
+			"runtime": soup.findAll('div', class_='ipc-metadata-list-item__content-container')[0].text,
+		}
 
 		list.append(data)
 	return list
@@ -37,4 +38,4 @@ def get_data(links, list):
 data = get_data(links, data)
 
 films_df = pd.DataFrame(data)
-films_df.to_csv('data.csv', index=False)
+films_df.to_csv('data.csv')
