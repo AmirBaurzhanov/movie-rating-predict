c188888fb5821ddbef302d34dfe7079f1d8377a7
buttman445 <amirbaurzhanov@gmail.com>
2022-09-27 21:12:11 +0600

    all data collected

--- parser/data_parser.py
+++ parser/data_parser.py
@@ -18,8 +18,9 @@ def get_data(links, list):
 		soup = bs(page.text, 'html.parser')
 		print(link)
 
+		actors = [soup.findAll('a', class_='sc-36c36dd0-1 QSQgP')[i].text for i in range(len(soup.findAll('a', class_='sc-36c36dd0-1 QSQgP')))]
 		all_writers = [soup.select('.ipc-metadata-list__item:nth-child(2) > div > ul > li > a')[i].text for i in range(len(soup.select('.ipc-metadata-list__item:nth-child(2) > div > ul > li > a'))-2)]
-		cert = None if len(soup.select('.sc-8c396aa2-2.itZqyK')) == 1 else soup.select('.sc-8c396aa2-2.itZqyK')[1].text
+		cert = None if len(soup.findAll('span', class_='sc-8c396aa2-2 itZqyK')) == 1 else soup.findAll('span', class_='sc-8c396aa2-2 itZqyK')[1].text
 		data = {
 			"title": soup.findAll('h1', class_='sc-b73cd867-0')[0].text,
 			"year": soup.findAll('a', class_='ipc-link ipc-link--baseAlt ipc-link--inherit-color sc-8c396aa2-1 WIUyh')[0].text,
@@ -27,8 +28,8 @@ def get_data(links, list):
 			"rating": soup.findAll('span', class_='sc-7ab21ed2-1 jGRxWM')[0].text,
 			"description": soup.findAll('span', class_='sc-16ede01-2 gXUyNh')[0].text,
 			"director": soup.select('li.ipc-metadata-list__item:first-child > div > ul > li > a')[0].text,
-			"writer": set(all_writers),
-			"actors": [soup.findAll('a', class_='sc-36c36dd0-1 QSQgP')[i].text for i in range(len(soup.findAll('a', class_='sc-36c36dd0-1 QSQgP')))],
+			"writer": all_writers,
+			"actors": actors,
 			"genres": soup.select('.ipc-chip-list__scroller')[0].text,
 			"runtime": None if soup.select('.sc-80d4314-2.iJtmbR > ul > li:nth-child(3)') == [] else soup.select('.sc-80d4314-2.iJtmbR > ul > li:nth-child(3)')[0].text
 		}
